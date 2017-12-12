e0113bbf3be589896e20e9ecb85961911ee07016
asus <1339166402@qq.com>
2017-12-12 20:06:33 +0800

    .......

---
+++ DouYu/app/src/main/java/com/example/asus/douyu/Home_page/Fragment1_scan.java
@@ -0,0 +1,15 @@
+package com.example.asus.douyu.Home_page;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.asus.douyu.R;
+
+public class Fragment1_scan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment1_scan);
+    }
+}
