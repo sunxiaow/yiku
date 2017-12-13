ab37ecfabc07d1325d008b52ab4af56acd99b984
asus <1339166402@qq.com>
2017-12-13 09:55:45 +0800

    上传

---
+++ YiKu/app/src/main/java/com/example/asus/yiku/Main2Activity.java
@@ -0,0 +1,13 @@
+package com.example.asus.yiku;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
