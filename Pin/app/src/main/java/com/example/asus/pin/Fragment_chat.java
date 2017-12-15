1eb2f5f4407f0a6375ef70fd1c4c2fff2104b1ca
asus <1339166402@qq.com>
2017-12-15 09:14:19 +0800

    ....

---
+++ Pin/app/src/main/java/com/example/asus/pin/Fragment_chat.java
@@ -0,0 +1,17 @@
+package com.example.asus.pin;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment_chat extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_fragment_chat,null);
+        return view;
+    }
+}
---
+++ Pin/app/src/main/java/com/example/asus/pin/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.asus.pin;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private List<Fragment> list = new ArrayList<>();
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewPager= (ViewPager) findViewById(R.id.viewpager);
+        list.add(new Fragment_home());
+        list.add(new Fragment_new());
+        list.add(new Fragment_search());
+        list.add(new Fragment_chat());
+        list.add(new Fragment_personal());
+        FragmentPagerAdapter fragmentPagerAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return list.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return list.size();
+            }
+        };
+        viewPager.setAdapter(fragmentPagerAdapter);
+    }
+}
